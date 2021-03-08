21aecac949a35ee6be722d66609a4e035e269aa4
yindl <ygzzjzzgy@163.com>
2021-03-07 22:33:19 -0800

    coroutine

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kotlin coroutine 总结
+
+# linux上配置多个git账号方法： https://blog.csdn.net/qq_35461287/article/details/55046835
+# linux 无法访问git网站 删除.ssh/known_hosts 文件 ，之后又重新生成一个文件。
+# linux 配置了多个git，免密之后无法push 到github报443错误，解决方法改用ssh方式的.git/config的方式即可。
--- app/src/main/java/com/hutong/relax/coroutine/MainActivity.kt
+++ app/src/main/java/com/hutong/relax/coroutine/MainActivity.kt
@@ -13,11 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(TAG, "主线程id：${mainLooper.thread.id}")
-        val job = GlobalScope.launch {
-            delay(6000)
-            Log.e(TAG, "协程执行结束-- 线程Id：${Thread.currentThread().id}")
-        }
+        Log.d(TAG, "主线程id：${mainLooper.thread.id}")
+//        GlobalScope.launch {  } 开启一个协程
+//        val job = GlobalScope.launch {
+//            delay(6000)
+//            Log.e(TAG, "协程执行结束-- 线程Id：${Thread.currentThread().id}")
+//        }
+
 //        asyncDemo()
 //        asyncDemo()
         //getResult2()
@@ -25,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
 //        syncFindBook1()
         syncFindBook2()
-        Log.e(TAG, "main Thread finished!")
+        Log.d(TAG, "main Thread finished!")
     }
 
     fun asyncDemo() {
@@ -37,50 +39,39 @@ class MainActivity : AppCompatActivity() {
         Log.e(TAG, "result= $res2 ,asyncDemo.threadid=${Thread.currentThread().id}")
     }
 
-    fun syncFindBook1() {
-        GlobalScope.launch {
-            var result = findBook(
-                "实习生小李",
-                { println(it + " threadid= ${Thread.currentThread().id}") })  // 异步数据,像同步代码一样return了
-            if (!result) {
-                result =
-                    findBook("老员工老王", { println(it + " threadid= ${Thread.currentThread().id}") })
-            }
-            if (!result) {
-                result =
-                    findBook("领班老赵", { println(it + " threadid= ${Thread.currentThread().id}") })
-            }
-            if (!result) {
-                result =
-                    findBook("仓库管理员", { println(it + " threadid= ${Thread.currentThread().id}") })
-            }
-            if (!result) {
-                result =
-                    findBook("扫地僧", { println(it + " threadid= ${Thread.currentThread().id}") })
-            }
-        }
-    }
-
     fun syncFindBook2() {
         GlobalScope.launch {
             var result = findBook2(
                 "实习生小李",
-                { println(it + " threadid= ${Thread.currentThread().id}") })  // 异步数据,像同步代码一样return了
+                {
+                    Log.d(
+                        TAG,
+                        it + " onError threadid= ${Thread.currentThread().id}"
+                    )
+                })  // 异步数据,像同步代码一样return了
             if (!result) {
                 result =
-                    findBook2("老员工老王", { println(it + " threadid= ${Thread.currentThread().id}") })
+                    findBook2(
+                        "老员工老王",
+                        { Log.d(TAG, it + " onError threadid= ${Thread.currentThread().id}") })
             }
             if (!result) {
                 result =
-                    findBook2("领班老赵", { println(it + " threadid= ${Thread.currentThread().id}") })
+                    findBook2(
+                        "领班老赵",
+                        { Log.d(TAG, it + " onError  threadid= ${Thread.currentThread().id}") })
             }
             if (!result) {
                 result =
-                    findBook2("仓库管理员", { println(it + " threadid= ${Thread.currentThread().id}") })
+                    findBook2(
+                        "仓库管理员",
+                        { Log.d(TAG, it + " onError  threadid= ${Thread.currentThread().id}") })
             }
             if (!result) {
                 result =
-                    findBook2("扫地僧", { println(it + " threadid= ${Thread.currentThread().id}") })
+                    findBook2(
+                        "扫地僧",
+                        { Log.d(TAG, it + " onError  threadid= ${Thread.currentThread().id}") })
             }
         }
     }
@@ -91,32 +82,16 @@ class MainActivity : AppCompatActivity() {
         return 2
     }
 
-    suspend fun findBook(name: String, onError: (String) -> Unit): Boolean {
-        return GlobalScope.async {
-            Log.e(TAG, "我是 $name 我开始找书,threadid= ${Thread.currentThread().id}")
-            val random = Random()
-            val seed = random.nextInt(10)
-            Thread.sleep((1000 * seed).toLong())
-            if (seed < 0) {
-                Log.e(TAG, "${name} 找到了,threadid= ${Thread.currentThread().id}")
-                true
-            } else {
-                onError("啊，${name} 没找到,threadid= ${Thread.currentThread().id}")
-                false
-            }
-        }.await()
-    }
-
     suspend fun findBook2(name: String, onError: (String) -> Unit): Boolean {
         return GlobalScope.async() {
-            Log.e(TAG, "我是 $name 我开始找书,threadid= ${Thread.currentThread().id}")
+            Log.d(TAG, "我是 $name 我开始找书,threadid= ${Thread.currentThread().id}")
             val random = Random()
             //产生一个0到9的随机数
             val seed = random.nextInt(10)
             //找书的时间是一个1-9秒的随机数
             Thread.sleep((1000 * seed).toLong())
             if (seed < 0) {//模拟一个找不到书的大概率事件
-                Log.e(TAG, "${TAG} 找到了,threadid= ${Thread.currentThread().id}")
+                Log.d(TAG, "${TAG} 找到了,threadid= ${Thread.currentThread().id}")
                 true  //lamdba表达式 true是返回值代表找到了书
             } else {
                 onError("啊,${name} 没找到书啊,threadid= ${Thread.currentThread().id}");
@@ -124,7 +99,6 @@ class MainActivity : AppCompatActivity() {
             }
         }.await()
     }
-
 
     companion object {
         val TAG = MainActivity::class.java.simpleName + "----"
